597fb8df315cbc4688d17a9dbeb8ff3ee2bddca6
Ayush Bastola <AB@webdev11.local>
2025-10-09 08:15:42 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("On one fateful day, your friend Edwinjames invites you over to his house.");
+        Thread.sleep(2000);
+        System.out.println("You arrive there and the both of you spend the day playing video games.");
+        Thread.sleep(2000);
+        System.out.println("Eventually, the both of you grow tired as the time comes closer to midnight. You both eventually pass out.");
+        Thread.sleep(2000);
+        System.out.println("When midnight is struck, however; you're mysteriously awoken to find Edwinjames missing.");
+        Thread.sleep(2000);
+        System.out.println("You get up and search for him, and there you find what once was your friend turned into a deadly animatronic.");
+        Thread.sleep(2000);
+        System.out.println("You will now have to survive against Edwinjames, using whatever is provided to you. Good luck.");
+    }
+}
